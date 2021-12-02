d44026f7725f26252b9a4dd84e38a3f35bcd691c
jripke74@gmail.com <jripke74@gmail.com>
2021-12-02 14:31:45 -0800

    add robots

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.bix",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "shanna@melissa.tv",
+  },
+  {
+    id: 3,
+    name: "Clemente Bauch",
+    username: "Smantha",
+    email: "Sincere@april.bix",
+  },
+  {
+    id: 4,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.bix",
+  },
+  {
+    id: 5,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.bix",
+  },
+  {
+    id: 6,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.bix",
+  },
+  {
+    id: 7,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.bix",
+  },
+  {
+    id: 8,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.bix",
+  },
+  {
+    id: 9,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.bix",
+  },
+];
