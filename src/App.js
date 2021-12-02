1d114b6f16c2c6f9f913f82566e7c7031b436996
jripke74@gmail.com <jripke74@gmail.com>
2021-12-02 13:29:53 -0800

    add Hello component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learning React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p className="App-intro">
+          To get started, edit <code>src/app.js</code> and save to reload.
+        </p>
       </div>
     );
   }
