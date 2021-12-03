d8111c5bdcdff6d864d28d16efbf7fae2bf8cd7d
jripke74@gmail.com <jripke74@gmail.com>
2021-12-03 10:54:39 -0800

    Updates

---
+++ precache-manifest.c503ff9f09d67b1cc6bc58cdec08eb59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b77eaf191d6592c96ab3f3da224b6e",
+    "url": "/comp-web-dev-0002/index.html"
+  },
+  {
+    "revision": "659357dc283c704ab700",
+    "url": "/comp-web-dev-0002/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "db68d54ba3641ca7241e",
+    "url": "/comp-web-dev-0002/static/css/main.545ee41e.chunk.css"
+  },
+  {
+    "revision": "659357dc283c704ab700",
+    "url": "/comp-web-dev-0002/static/js/2.e1b685fc.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/comp-web-dev-0002/static/js/2.e1b685fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db68d54ba3641ca7241e",
+    "url": "/comp-web-dev-0002/static/js/main.5270303e.chunk.js"
+  },
+  {
+    "revision": "46d6dda3679172e90508",
+    "url": "/comp-web-dev-0002/static/js/runtime-main.f722ccc3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/comp-web-dev-0002/static/media/SEGA.12a3db8b.woff"
+  }
+]);
