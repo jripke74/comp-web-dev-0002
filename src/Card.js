0d5cb098776a6772c5ef5de83a4f354e2900fa53
Jeff Ripke <jripke74@gmail.com>
2021-12-02 16:05:47 -0800

    add prop for list of robots

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-const Card = (props) => {
-  const { name, email, id}
+const Card = ({ name, email, id }) => {
   return (
-    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
       <div>
         <h2>{name}</h2>
@@ -11,6 +10,6 @@ const Card = (props) => {
       </div>
     </div>
   );
-}
+};
 
 export default Card;
